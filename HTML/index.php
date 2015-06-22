42cef9a6be644c79f71f0006da4397d450acb5ab
Maciej Kozik <kamac2495@gmail.com>
2015-06-22 21:14:35 +0200

    add 'toggle internet gaps' functionality

--- HTML/index.php
+++ HTML/index.php
@@ -27,14 +27,19 @@ $pingTargets = scandir("../pings/");
         }
     </style>
 
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
     <!-- note: moment.js must be loaded before vis.js, else vis.js uses its embedded version of moment.js -->
     <script src="js/moment.min.js"></script>
     <script src="js/visjs/vis.min.js"></script>
+    <script src="js/jquery-2.1.4.min.js"></script>
     <link href="js/visjs/vis.min.css" rel="stylesheet" type="text/css" />
     <link href="main.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
+
+<div class="menuBtn noselect" name="internetGaps">toggle internet gaps</div>
+
 <div id="visualization" style="width:100%"></div>
 
 <script>
@@ -47,6 +52,7 @@ $pingTargets = scandir("../pings/");
             echo "groups.add({id:" . $group . ",content:\"" . $pingTarget . "\"});";
             $group++;
         }
+        echo "var groupCount = " . $group . ";";
     ?>
     // create a dataset with items
     var now = moment().minutes(0).seconds(0).milliseconds(0);
@@ -58,18 +64,24 @@ $pingTargets = scandir("../pings/");
     var endPoint = moment().endOf('day').minutes(0).seconds(0).milliseconds(0);
 
     var container = document.getElementById('visualization');
-    var options = {
+    var displayInternetGaps = false;
+    var visOptions = {
         defaultGroup: 'ungrouped',
         legend: true,
         sampling: true,
-        drawPoints: {enabled:false, size:3},
+        drawPoints: {enabled:false, size:0},
         catmullRom: false,
         height: window.innerHeight + "px",
         start: startPoint,
-        end: endPoint
+        end: endPoint,
+        groups: {
+        	visibility: {
+        		9: displayInternetGaps
+        	}
+        }
     };
 
-    dataset.add([<?php
+    pingData = [<?php
         $group = 0;
         $str = "";
         $id = 0;
@@ -94,9 +106,111 @@ $pingTargets = scandir("../pings/");
 
         echo substr($str, 0, strlen($str) - 1);
 
-    ?>]);
+    ?>];
+    var lastID = <?php echo $id; ?>;
 
-    var graph2d = new vis.Graph2d(container, dataset, groups, options);
+    dataset.add(pingData);
+
+    var graph2d;
+
+    function getGaps(groupID) {
+    	// look for the lack of internet accessibility fields
+    	var gaps = [];
+    	var currGap = null;
+    	for(var i=0; i < pingData.length; i++) {
+    		if(i != pingData.length-1 && pingData[i].group == groupID && (pingData[i].y > 300 || pingData[i].y == 0)) {
+    			if(currGap == null) {
+    				currGap = {beginX:pingData[i].x, beginID:pingData[i].id}
+    			}
+    		} else if(currGap != null) {
+    			currGap.endX = pingData[i].x;
+    			gaps.push(currGap);
+    			currGap = null;
+    		}
+    	}
+    	// merge close gaps
+    	var mergedGaps = [];
+    	for(var i=0; i < gaps.length; i++) {
+    		var currentGap = gaps[i];
+    		if(i < (gaps.length-1))
+    		{
+	    		var nextGap = gaps[i+1];
+	    		do
+	    		{
+		    		if(nextGap.beginX - currentGap.endX < 20000) {
+		    			currentGap.endX = nextGap.endX;
+		    			i++;
+		    			if(i < gaps.length-1) {
+		    				nextGap = gaps[i+1];
+		    			} else {
+		    				break;
+		    			}
+		    		} else {
+		    			break;
+		    		}
+		    	} while(true);
+		   	}
+	    	mergedGaps.push(currentGap);
+    	}
+    	// get rid of too small gaps
+    	gaps = [];
+    	for(var i=0; i < mergedGaps.length; i++) {
+    		if(mergedGaps[i].endX - mergedGaps[i].beginX > 20000) {
+    			gaps.push(mergedGaps[i]);
+    		}
+    	}
+    	return gaps;
+    }
+
+    function msToTime(s) {
+		var ms = s % 1000;
+		s = (s - ms) / 1000;
+		var secs = s % 60;
+		s = (s - secs) / 60;
+		var mins = s % 60;
+		var hrs = (s - mins) / 60;
+
+		if(secs >= 30) {
+			mins++;
+		}
+		var out = "";
+		if(hrs != 0) {
+			out = out + hrs + "h ";
+		}
+		if(mins != 0) {
+			out = out + mins + "m ";
+		}
+		return "~" + out;
+	}
+
+    $(document).ready(function() {
+    	groups.add({id:9, content:"internet gap", options:{shaded:{orientation:'bottom'}, drawPoints: {enabled:true, size:0}}});
+    	var remoteGaps = getGaps(1);
+    	var gapData = [];
+    	gapData.push({id:lastID++,x:startPoint,y:-1,group:9});
+    	for(var i = 0; i < remoteGaps.length; i++) {
+    		var p = msToTime(remoteGaps[i].endX - remoteGaps[i].beginX);
+    		var pingL = {
+		    	content:p, xOffset:1, yOffset:20, className: "pLabel"
+		    }
+    		gapData.push({id:lastID++,x:remoteGaps[i].beginX-1,y:-1,group:9});
+    		gapData.push({id:lastID++,x:remoteGaps[i].beginX,y:999,group:9,label:pingL});
+    		gapData.push({id:lastID++,x:remoteGaps[i].endX,y:999,group:9});
+    		gapData.push({id:lastID++,x:remoteGaps[i].endX+1,y:-1,group:9});
+    	}
+    	gapData.push({id:lastID++,x:endPoint,y:-1,group:9});
+    	dataset.add(gapData);
+
+    	graph2d = new vis.Graph2d(container, dataset, groups, visOptions);
+
+
+    	$(".menuBtn[name=internetGaps]").click(function() {
+    		displayInternetGaps = !displayInternetGaps;
+    		visOptions.groups.visibility = {9:displayInternetGaps};
+    		graph2d.setOptions(visOptions);
+    	});
+    });
+
 </script>
 </body>
 </html>
